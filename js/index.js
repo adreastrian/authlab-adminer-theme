2e79043fba9acf083d065026f286420de15a56da
Mahmudul Hasan <mahmudulhasanarif@gmail.com>
2025-07-02 18:37:28 +0600

    cleanup

--- js/index.js
+++ js/index.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
     $('.footer').css({
         minWidth: $('#content table').first().width() + 20 + 'px'
     });
+
+    // js determine if url has import
+    if (window.location.href.includes('import')) {
+        $("body").addClass('import');
+    }
+    
+    if (window.location.href.includes('&select')) {
+        $("body").addClass('select-data-page');
+    }
 });
